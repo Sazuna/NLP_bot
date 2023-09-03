ff4ecbd71a37345758cb5f170109595bd2e26475
Sazuna <liza.fretel@gmail.com>
2023-09-04 01:56:35 +0200

    Commande de preprocessing (le reste n'est pas encore fonctionnel)

--- script/reponses.py
+++ script/reponses.py
@@ -1,4 +1,12 @@
+import sys
+sys.path.insert(1, 'recherche_message/')
+import requete
+
 def reponds(message):
+	if message.strip()[:5] == "query":
+		ir.query(message[6:])
+	if message.strip()[:10] == "preprocess":
+		return requete.preprocess_message(message[10:].strip())
 	if message == "j'en ai marre":
 		return "moi aussi :("
 	if message == "Ping":
